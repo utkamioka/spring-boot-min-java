f39f207d23845573669bd95e0e84d9480d4b75eb
Yutaka Kamioka <yutaka.kamioka@jp.fujitsu.com>
2017-09-29 16:15:32 +0900

    Modified: use message.properties when specified unknown accept-language

--- src/main/java/com/example/kamioka/HelloController.java
+++ src/main/java/com/example/kamioka/HelloController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -19,23 +20,34 @@ public class HelloController {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss zzz");
 
     @RequestMapping(path = "/hello")
-    public String hello(Model model, HttpServletRequest req) {
+    public String hello(Model model, HttpServletRequest req, HttpServletResponse resp) {
         System.out.println(this.getClass().getPackage().getName() + ".HelloController.hello()");
         model.addAttribute("date", ZonedDateTime.now().format(FORMATTER));
-        model.addAttribute("request_id", req.getAttribute("Request-Id"));
+        model.addAttribute("request_id", resp.getHeader("Request-Id"));
         return "hello";
     }
 
     @RequestMapping(path = "/hello-submit")
-    public String helloSubmit(@ModelAttribute("foo") String foo, Model model, HttpServletRequest req) {
+    public String helloSubmit(@ModelAttribute("foo") String foo, Model model, HttpServletRequest req, HttpServletResponse resp) {
+        System.out.println(this.getClass().getPackage().getName() + ".HelloController.helloSubmit()");
         model.addAttribute("date", foo);
-        model.addAttribute("request_id", req.getAttribute("Request-Id"));
+        model.addAttribute("request_id", resp.getHeader("Request-Id"));
         return "hello";
     }
 
-    @RequestMapping(path = "/neko", produces = MediaType.TEXT_PLAIN_VALUE)
+    @RequestMapping(path = "/neko", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
     @ResponseBody
     public Resource neko() {
+        System.out.println(this.getClass().getPackage().getName() + ".HelloController.neko()");
+        // なにもしないと、Content-Typeが"application/json"になるので、"application/octet-stream"を指定する。
+        return new ClassPathResource("wagahaiwa_nekodearu");
+    }
+
+    @RequestMapping(path = "/neko.txt")
+    @ResponseBody
+    public Resource neko_txt() {
+        System.out.println(this.getClass().getPackage().getName() + ".HelloController.neko_txt()");
+        // "*.txt"だと、Content-Typeが強制的に"text/plain"になる。
         return new ClassPathResource("wagahaiwa_nekodearu.txt");
     }
 }
